94b7590eb803b9667a063af59fa841a8e466f140
alex1234567639 <alex8603062000@gmail.com>
2021-05-01 21:37:03 +0800

    [feature]抓取 PTT 八卦版文章-cookie

---
+++ ptt-gossiping-title.py
@@ -0,0 +1,26 @@
+# Python 2.7.9 之後引入了一个新特性，當使用urllib.urlopen打開一個 https 鏈接時，
+# 會驗證一次 SSL，而當目標網站使用的是自簽名的證書時就會拋出異常
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# 抓取 PTT 八卦版的網頁原始碼 (注意有Cookie)
+import urllib.request as req
+url = "https://www.ptt.cc/bbs/Gossiping/index.html"
+
+# 建立一個 Request 物件，附加 Request Headers 的資訊
+request = req.Request(url, headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+    "cookie": "over18=1"
+})
+
+with req.urlopen(request) as response:
+    data = response.read().decode("utf-8")
+
+# 解析原始碼，取得每篇的文章標題
+import bs4
+root = bs4.BeautifulSoup(data, "html.parser") # 將抓到的hrml丟給bs4解析
+
+titles = root.find_all("div", class_="title") # 尋找所有 class="title" 的 div 標籤
+for title in titles:
+    if title.a != None:
+        print(title.a.string)
